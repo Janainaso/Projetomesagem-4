530e2a75e0bec565d7d652fc538dc5a713b68b9a
4551721203 <32827359-4551721203@users.noreply.replit.com>
2024-03-15 09:46:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#soma
+soma_resultado=10+5
+print("soma:",soma_resultado)
+
+#subtração
+subtracao_resultado=10-5
+print("subtração:", subtracao_resultado)
+
+#multiplicação
+multiplicacao_resultado=10*5
+print("multiplicação:", multiplicacao_resultado)
+
+#divisão
+divisao_resultado=10/5
+print("divisão:", divisao_resultado)
